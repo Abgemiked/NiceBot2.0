11811ad46313282540818d8c748fef3227f655b2
Abgemiked <fabisch101@googlemail.com>
2023-07-05 14:31:26 +0200

    Update leave_log.py

--- events/logs/leave_log.py
+++ events/logs/leave_log.py
@@ -2,7 +2,7 @@ import discord
 
 async def handle_member_remove(member, bot, LEAVE_LOG_CHANNEL_ID):
     log_channel = bot.get_channel(LEAVE_LOG_CHANNEL_ID)
-    await log_channel.send(f'{member.display_name} hat den Server verlassen.')
+    await log_channel.send(f'**{member.display_name}** hat den Server verlassen.')
 
 
 async def on_member_remove_handler(member, bot, LEAVE_LOG_CHANNEL_ID):
